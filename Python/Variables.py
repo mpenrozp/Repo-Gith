7259ec76578e7bdc44d29cb68141d474524aa0c7
mauricio penroz <mpenrozp@directv.cl>
2022-07-19 12:23:18 -0400

    .

---
+++ Python/Variables.py
@@ -0,0 +1,14 @@
+from operator import truediv
+
+
+nombre = "Mau"
+edad = 32
+estatura=1.73
+positivo=True
+negativo=False
+
+print(type(nombre))
+print(type(edad))
+print(type(estatura))
+print(type(positivo))
+print(type(negativo))
