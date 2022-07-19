612e802c70e4f40e7f1ee344c8a03f63dbae8f30
mauricio penroz <mpenrozp@directv.cl>
2022-07-19 12:40:23 -0400

    se cambia de nombre archivo

---
+++ Python/listas.py
@@ -0,0 +1,6 @@
+lenguajes =["python","java","c#"]
+print(lenguajes)
+print(lenguajes[0])#devuelve primer elemento
+print(len(lenguajes)) #imprime el largo
+print(lenguajes[-2]) #devuelve segundo elemento
+print(lenguajes[0:2]) #devuelve lo que esta entre el primer y segundo elemento
