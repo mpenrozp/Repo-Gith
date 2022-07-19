7372936b453540137d2dd38d4c054643f7fc011c
mauricio penroz <mpenrozp@directv.cl>
2022-07-19 16:23:43 -0400

    .

---
+++ Python/condiciones.py
@@ -0,0 +1,19 @@
+a=2
+b=2
+c=2
+if a<b:
+    print("a es menor que b")
+elif a==b:
+    print("a es igual que b")
+else:
+    print("a es mayor que b")
+
+if c == True:
+    print("c es verdadero")
+else:
+    print("c es falso")
+
+if type(c) is bool:
+    print("c es bool")
+else:
+    print("c es otro tipo de dato")
