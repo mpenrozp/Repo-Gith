328013e1681a1d353eac2ff715294bbea58b9e82
mauricio penroz <mpenrozp@directv.cl>
2022-07-19 16:41:34 -0400

    for

---
+++ Python/ciclos_for.py
@@ -0,0 +1,12 @@
+for letra in "texto":
+   if letra == "t":
+      continue
+   print(letra)
+
+lenguajes =["python","java","c#"]
+for lenguaje in lenguajes:
+    print(lenguaje)
+
+for element in range(5):
+    print(element)
+    
