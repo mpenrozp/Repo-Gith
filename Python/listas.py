c2aa5e73132e75a89379e3b085a70ff62b141a3d
mauricio penroz <mpenrozp@directv.cl>
2022-07-19 12:44:54 -0400

    otro cambio

--- Python/listas.py
+++ Python/listas.py
@@ -4,3 +4,6 @@ print(lenguajes[0])#devuelve primer elemento
 print(len(lenguajes)) #imprime el largo
 print(lenguajes[-2]) #devuelve segundo elemento
 print(lenguajes[0:2]) #devuelve lo que esta entre el primer y segundo elemento
+
+lista = [lenguajes,"practica","dedicación"]
+print(lista) 
